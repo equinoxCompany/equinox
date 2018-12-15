4d9b3e3e10d2b43d76baa30272d8f91930af9b62
Vlad Kalinichenko <mynubmer0994212154@gmail.com>
2018-12-15 21:59:04 +0200

    mobile_service

--- src/Components/Services/ServicesMenu.js
+++ src/Components/Services/ServicesMenu.js
@@ -6,6 +6,9 @@ import Map from '../../media/map.png';
 import Socials from '../Socials';
 import '../../Styles/ServicesMenu.css';
 import MobileBlogNav from '../MobileBlogNav';
+import MobileNav from '../MobileNav';
+import MobileMap from '../../media/mobile_map.png';
+import MobileServiceImg from '../../media/service_img.png'
 
 export default class extends Component {
   constructor(props){
@@ -89,21 +92,22 @@ export default class extends Component {
       </div>
       <Socials/>
       </div>):(
-        <div className="mobile-services-menu">
-          <div className="serivices-menu-openmap">Open map</div>
-          <div className="mobile-services-menu-header"></div>
-          <div className="services-menu-title">SERVICE TITLE</div>
-          <div className="services-menu-img">
-            <div className="services-menu-img-main"></div>
-            <div className="services-menu-img-subs">
-              <div className="services-menu-img-sub"><h1>Subtype</h1></div><br/>
-              <div className="services-menu-img-sub"><h1>Subtype</h1></div>
-            </div> 
-          </div>
-          <div className="services-menu-text">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.z
-          </div>
-          {/* <MobileNav/> */}
+        <div className="m_service_page">
+          <MobileNav/>
+          <section className="m_service_info">
+            <h3>Open full map</h3>
+            <img src={MobileMap} className="m_services_map"/>
+            <h1>SERVICE TITLE</h1>
+            <img src={MobileServiceImg} className="m_service_img"/>
+            <p>Let’s face it: the digital age has introduced 
+a bunch of new players on the block. 
+Hey, you might be literally going up against
+a computer. That’s where we comes 
+in–our team has winning visual and 
+interactive combos at our fingertips to take 
+brands and businesses to the next level, 
+and we have fun doing it.</p>
+          </section>
         </div>
       )
     }
