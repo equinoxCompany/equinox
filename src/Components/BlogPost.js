718299df7826fdb725c583ce0996401fed32fead
Vlad Kalinichenko <mynubmer0994212154@gmail.com>
2019-01-05 15:04:00 +0200

    'fix_blog'

--- src/Components/BlogPost.js
+++ src/Components/BlogPost.js
@@ -4,17 +4,16 @@ import {Link} from 'react-router-dom';
 
 const Post = (props) => {
   return(
-    <div className="post">
-    <Link to={{pathname: '/post/'+props.item.url, state:{id: props.item._id}}} className="post">
-      <div className="subject">
+    <div>
+    <Link to={{pathname: '/post/'+props.item.url, state:{id: props.item._id}}} className="blog_post">
+      <div className="blog_post_subject">
         <time className="date" datetime={props.item.date}>{props.item.date.slice(5, 10).replace('-', '.')}</time>
         <div className="post_title">
           <h2>{props.item.title}</h2><br/>
         </div>
       </div>
-      <div className="post_preview" style={{backgroundImage:`url(${'http://91.225.165.43:3001/'+props.item.postImage})`, backgroundRepeat:'no-repeat', backgroundSize:'cover'}}>
-        <div className="cover"></div>
-        <div className="post_info">
+      <div className="blog_post_preview" style={{backgroundImage:`url(${'http://91.225.165.43:3001/'+props.item.postImage})`, backgroundRepeat:'no-repeat', backgroundSize:'cover'}}>
+        <div className="blog_post_info">
           <div className="short_info">
             {props.item.preview_text && props.item.preview_text}
           </div>
